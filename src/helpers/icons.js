397c68866fa7a435601f28fc8cb6a8ee4ea4a2e0
Agustin Barrionuevo <barrionuevoagustin289@gmail.com>
2023-06-01 17:37:43 -0300

    icons and normalize css

---
+++ src/helpers/icons.js
@@ -0,0 +1,15 @@
+
+export { default as CSS } from "../assets/icons/CSS.png";
+export { default as HTML } from "../assets/icons/HTML.png";
+export { default as JavaScript } from "../assets/icons/Javascript.png";
+export { default as NodeJS } from "../assets/icons/NodeJS.png";
+export { default as ExpressJS } from "../assets/icons/Express.png";
+export { default as NPM } from "../assets/icons/NPM.png";
+export { default as PostgreSQL } from "../assets/icons/Postgresql.png";
+export { default as MongoDB } from "../assets/icons/MongoDB.png";
+export { default as React } from "../assets/icons/React.png";
+export { default as Redux } from "../assets/icons/Redux.png";
+export { default as Sequelize } from "../assets/icons/Sequelize.png";
+export { default as Git } from "../assets/icons/Git.png";
+export { default as GitHub } from "../assets/icons/GitHub.png";
+export { default as Bootstrap } from "../assets/icons/Bootstrap.png";
