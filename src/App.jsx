d205f3e55f5b89cf6efa94892c79666c35c975ce
Agustin Barrionuevo <barrionuevoagustin289@gmail.com>
2023-06-15 18:22:24 -0300

    background and css layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,30 @@ import img from './assets/Yo2.png'
 import './App.css'
 
 function App() {
+  // const box = document.querySelector('.background')
+  // const boxContent = document.querySelector('.blur')
+  // const sensitivity = 15
+
+  // box.addEventListener('mousemove', addCoors)
+  // boxContent.addEventListener('mousemove', addCoors)
+
+  // function addCoors(e){
+  //   const x = e.clientX;
+  //   const y = e.clientY
+  //   box.style.backgroundPosition = `${x / sensitivity}px ${y / sensitivity}px`
+  // }
   return (
-    <div className="crop">
-      <NavLink to='/'><img src={img} className="me"/></NavLink>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" Component={About} />
-        <Route exact path="Skills" Component={Skills} />
-        <Route exact path="Projects" Component={Projects} />
-        <Route exact path="ContactMe" Component={ContactMe} />
-      </Routes>
+    <div className="background">
+      <div className="blur">
+        <NavLink to='/'><img src={img} className="me"/></NavLink>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" Component={About} />
+          <Route exact path="Skills" Component={Skills} />
+          <Route exact path="Projects" Component={Projects} />
+          <Route exact path="ContactMe" Component={ContactMe} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,9 +8,9 @@ export default function About () {
 	return(
 		<div className='about-container'>
 			<div className='about'>
-				<h1>Full-Stack Developer</h1>
-				<h2>I'm Agustin Barrionuevo, a Full-Stack Developer and Business student based in Argentina. </h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem nostrum ex excepturi ad animi voluptates, quos earum iure molestiae accusantium reprehenderit, voluptate mollitia. Libero, et quo delectus magni soluta quod! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel non cupiditate, impedit veritatis necessitatibus culpa consequatur excepturi obcaecati molestiae officia soluta, repellat deleniti tenetur modi quidem minima, corporis aut voluptas!</p>
+				<h1 className='title'>Full-Stack Developer</h1>
+				<h2 className='subtitle'>I'm Agustin Barrionuevo, a Full-Stack Developer and Business student based in Argentina. </h2>
+				<p className='text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem nostrum ex excepturi ad animi voluptates, quos earum iure molestiae accusantium reprehenderit, voluptate mollitia. Libero, et quo delectus magni soluta quod! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel non cupiditate, impedit veritatis necessitatibus culpa consequatur excepturi obcaecati molestiae officia soluta, repellat deleniti tenetur modi quidem minima, corporis aut voluptas!</p>
 			</div>
 			<div className='social-links'>
 				<button className='link-btn'><a className='link' href='https://www.linkedin.com/in/agustin-barrionuevo-a7a25b238/'L target='_blank'><BsLinkedin/></a></button>
